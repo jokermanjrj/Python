97ca8b72fd2e96212342e3b4dd92fe315cd8c922
ruben <jokermanjrj@gmail.com>
2019-11-25 21:06:27 +0100

    0.0.2

--- Practica_Final/venv/Main.py
+++ Practica_Final/venv/Main.py
@@ -13,6 +13,19 @@ def identificarse():
     nombre=input("Nombre : ")
     contraseña=input("Contraseña : ")
 
+    #Comprobamos el si el nombre y la contraseña existe y si es admin
+    contador = 1
+    while contador < len(usuarios):
+        print(usuarios.get(contador)[0], usuarios.get(contador)[2])
+        if usuarios.get(contador)[0] == nombre and usuarios.get(contador)[2] == contraseña:
+            if usuarios.get(contador)[4] == 'false':
+                print("Usuario normal logueado correctamente")
+                break;
+            elif usuarios.get(contador)[4] == 'true':
+                print("Usuario administrador logueado correctamente")
+                break;
+        contador = contador + 1
+
 
 def listar():
     print("Ver todos los productos")
@@ -21,7 +34,16 @@ def listar():
 def salir():
     sys.exit()
 
+##############
+#Diccionarios#
+##############
 
+#Diccionario Usuarios
+
+
+usuarios = {1: ['ruben', 'martinez', 'admin', '20', 'true', [1, 2]],
+            2: ['ivan', 'clemente', 'admin', '20', 'false', [1]]
+            }
 
 #######################
 #Menu usuario anónimos#
@@ -44,3 +66,4 @@ elif opcion == 3:
 ############################
 #Menu usuario Identificados#
 ############################
+
