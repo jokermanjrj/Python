ef550e7a48a254468fd1de903d39a129afeef690
ruben <jokermanjrj@gmail.com>
2019-11-27 21:09:31 +0100

    0.0.3

--- Practica_Final/venv/Main.py
+++ Practica_Final/venv/Main.py
@@ -5,7 +5,6 @@
 ############
 import sys
 
-
 #######################################
 #Funciones de Acciones de los usuarios#
 #######################################
@@ -15,20 +14,51 @@ def identificarse():
 
     #Comprobamos el si el nombre y la contraseña existe y si es admin
     contador = 1
-    while contador < len(usuarios):
-        print(usuarios.get(contador)[0], usuarios.get(contador)[2])
+    #Empieza en no logeado por defecto ni en usuario ni en administrado
+
+    while contador < len(usuarios)+1:
         if usuarios.get(contador)[0] == nombre and usuarios.get(contador)[2] == contraseña:
             if usuarios.get(contador)[4] == 'false':
-                print("Usuario normal logueado correctamente")
-                break;
+                #Si a pasado el logueo de usuario cambiamos la variable login a true
+                print("Usuario normal logueado correctamente\n")
+                menu_registrado()
             elif usuarios.get(contador)[4] == 'true':
-                print("Usuario administrador logueado correctamente")
-                break;
+                print("Usuario administrador logueado correctamente\n")
+                menu_admin()
         contador = contador + 1
+
+#########################
+#Menu usuario Registrado#
+#########################
+def menu_registrado():
+    print('Bienvenido señor\n')
+    print("Menu de usuario anónimo\n"
+          "1 : Modificar tus Datos\n"
+          "2 : Mostrar tus vendedores recomendados\n"
+          "3 : Recomendar un vendedor\n"
+          "4 : Ver todos los productos a la venta\n"
+          "5 : Ver productos de tus vendedores recomendados\n"
+          "6 : Valorar producto\n"
+          "7 : Poner a la venta un producto\n"
+          "8 : Desconectarse (Volverse anónimo)\n"
+          "9 : Salir de la aplicación\n"
+          "------------------------------------------------\n"
+          )
+    opcion = int(input("Opción : "))
+
+    if opcion == 1:
+        print()
+
+
+def menu_admin():
+    print('Bienvenido esclavo')
 
 
 def listar():
-    print("Ver todos los productos")
+    contador = 1
+    while contador < len(productos)+1:
+        print(productos.get(contador)[0])
+        contador = contador + 1
 
 
 def salir():
@@ -45,6 +75,12 @@ usuarios = {1: ['ruben', 'martinez', 'admin', '20', 'true', [1, 2]],
             2: ['ivan', 'clemente', 'admin', '20', 'false', [1]]
             }
 
+#Diccionario Productos
+
+
+productos = {1: ['1', 'casco', 'seminuevo', '2', [1, 2], [1, 2], [1, 2]],
+             2: ['2', 'ivan', 'nuevo', '1', [1, 2], [1, 2], [1]]
+             }
 #######################
 #Menu usuario anónimos#
 #######################
@@ -62,8 +98,4 @@ elif opcion == 2:
     listar()
 elif opcion == 3:
     salir()
-
-############################
-#Menu usuario Identificados#
-############################
 
